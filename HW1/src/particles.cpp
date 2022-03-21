9ab6c79bd28cf7c5e7ccb0bcff3cf401fdedc2cb
TzuMinYang <sam1248105@gmail.com>
2022-03-22 01:54:13 +0800

    HW1 2st繳交, 改了角速度的initial

--- HW1/src/particles.cpp
+++ HW1/src/particles.cpp
@@ -1,16 +1,20 @@
 #include "particles.h"
 
 Particles::Particles(int size, float mass_) noexcept :
-    _position(4, size), _velocity(4, size), _acceleration(4, size), _mass(size, mass_) {
+    _position(4, size), _velocity(4, size), _acceleration(4, size), _angular_velocity(4, size), _angular_acceleration(4, size), _mass(size, mass_) {
   _position.setZero();
   _velocity.setZero();
   _acceleration.setZero();
+  _angular_velocity.setZero();
+  _angular_acceleration.setZero();
 }
 
 void Particles::setZero() {
   _position.setZero();
   _velocity.setZero();
   _acceleration.setZero();
+  _angular_velocity.setZero();
+  _angular_acceleration.setZero();
 }
 
 void Particles::resize(int newSize) {
